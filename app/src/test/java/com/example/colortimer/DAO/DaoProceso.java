d34e0b61555f38108b4d9a04d7b2f41b64bd5a51
Cesar Ramirez <Cesar.Ram.30020@gmail.com>
2023-03-22 23:40:17 -0600

    SQL DaoProcesos

--- app/src/test/java/com/example/colortimer/DAO/DaoProceso.java
+++ app/src/test/java/com/example/colortimer/DAO/DaoProceso.java
@@ -1,13 +1,39 @@
 package com.example.colortimer.DAO;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
 import com.example.colortimer.Datos.Proceso;
 
 import java.util.ArrayList;
 
-public class DaoProceso {
+public class DaoProceso extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "ColorTimer.db";
+    private static final String TABLE_PROCESOS = "Contactos";
 
-    public DaoProceso(){
+    public DaoProceso(@Nullable Context context) {
         // Aqui deberemos hacer la conexion a la base de datos
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE "+TABLE_PROCESOS+"(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "tiempoDecoloracion INTEGER," +
+                "estado VARCHAR(1))"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE "+TABLE_PROCESOS);
+        this.onCreate(sqLiteDatabase);
     }
 
     /**
