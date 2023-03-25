bae32c8d79f8c7b6defb5c3fde0ea5f57b927cee
Josafat Jauregui <alfonso.lopez5658@alumnos.udg.mx>
2023-03-25 00:05:15 -0600

    A ver, esta mamada ya toma foto. No entiendo por qué no lo almacena en el bitmap. Queda esta noche si o si

--- app/src/main/java/com/example/colortimer/MainActivity.java
+++ app/src/main/java/com/example/colortimer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.colortimer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -11,6 +13,8 @@ import com.example.colortimer.DAO.DaoProceso;
 import com.example.colortimer.DAO.DaoTinte;
 import com.example.colortimer.Datos.Proceso;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -20,6 +24,7 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -29,28 +34,46 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS= 7;
-    static final int REQUEST_TAKE_PHOTO = 1;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+
     private String currentPhotoPath;
     private ImageView img;
     private Button btnProcesos;
+
+    private Button btn;
+
     private DaoCabello dbCabello;
     private DaoColor dbColor;
     private DaoProceso dbProceso;
     private DaoTinte dbTinte;
+
+
+    private  int CAMERA_PERMISION_CODE = 1;
+    private  int CAMERA_REQUEST_CODE = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = findViewById(R.id.imageView);
-        btnProcesos = findViewById(R.id.button2);
-        dbColor = new DaoColor(this);
-        dbTinte = new DaoTinte(this);
-        dbProceso = new DaoProceso(this);
-        dbCabello = new DaoCabello(this);
+        btn = findViewById(R.id.btn_camera);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(
+                        view.getContext(),
+                        android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{android.Manifest.permission.CAMERA},
+                            CAMERA_PERMISION_CODE);
+                }
+            }
+        });
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -60,62 +83,52 @@ public class MainActivity extends AppCompatActivity {
             }, 1000);
         }
 
+
+        img = findViewById(R.id.iv_image);
+
+
+        btnProcesos = findViewById(R.id.button2);
+        dbColor = new DaoColor(this);
+        dbTinte = new DaoTinte(this);
+        dbProceso = new DaoProceso(this);
+        dbCabello = new DaoCabello(this);
+
+
+
         /*Proceso p = new Proceso();
         p.setId((int) dbProceso.crear(p));
-        Toast.makeText(MainActivity.this,"ID: "+p.getId(),Toast.LENGTH_SHORT).show();*/
-    }
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "Backup" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
+        Toast.makeText(MainActivity.this,"ID: "+p.getId(),Toast.LENGTH_SHORT).show();
 
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-    private void dispatchTakePictureIntent() { //Metodo para tomar fotografias
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
 
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
+
+        */
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            img.setImageBitmap(imageBitmap);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == CAMERA_PERMISION_CODE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }else{
+                Toast.makeText(this,
+                        "Sin permisos. Otorgalos desde configuracion",
+                        Toast.LENGTH_LONG);
+            }
         }
     }
-    public void abreProcesosActivity(View view){
-        try{
-            Intent aProcesos = new Intent(MainActivity.this,ProcesosActivity.class);
-            startActivity(aProcesos);
-        } catch (Exception e){
-            Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == Activity.RESULT_OK){
+            if(requestCode == CAMERA_REQUEST_CODE){
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                img.setImageBitmap(imageBitmap);
+            }
         }
     }
 }
